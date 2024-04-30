71fcba3e7bd62458fdf60dc68e07b9dfd4f90af5
devgarg <dev.garg@nagarro.com>
2024-04-30 14:53:26 +0530

    Commit

--- src/main/java/com/nagarro/DevOpsAssignment/controller/BasicController.java
+++ src/main/java/com/nagarro/DevOpsAssignment/controller/BasicController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.nagarro.devOpsAssignment.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
